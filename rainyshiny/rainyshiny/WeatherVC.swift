8b4a14eb5fa571c20530d53c12388674f009e4b3
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-08 16:40:58 -0600

    Update Views/Implement DataSource/Delegate Functions

--- rainyshiny/rainyshiny/WeatherVC.swift
+++ rainyshiny/rainyshiny/WeatherVC.swift
@@ -8,10 +8,32 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set Delegate/Datasource
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    //MARK: - UITableViewDataSource/Delegate Functions
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        
+        return cell
     }
     
     
